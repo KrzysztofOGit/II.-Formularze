3656be56276a0ad9c7a69ab782c6ebe9dffb9e1c
ogrodek <70914232+KrzysztofOGit@users.noreply.github.com>
2021-12-04 21:38:30 +0000

    function add book and css for ul update

--- script.js
+++ script.js
@@ -32,9 +32,30 @@ for(let key in tabBooks){
 	document.getElementById("showCategory").appendChild(liCategory);
 }
 
-//dodanie nowej ksiazki do listy
+//wyswietlenie nowej ksiazki do listy
 function addBooks(){
-			
+	let i = tabBooks.length-1;
+	let lastBook = tabBooks[i];
+
+	let liTitle = document.createElement("li");
+	let liAuthor = document.createElement("li");
+	let liPriority = document.createElement("li");
+	let liCategory = document.createElement("li");
+
+	let showValueTitle = document.createTextNode(tabBooks[i].valueBookTitle);
+	let showValueAutor = document.createTextNode(tabBooks[i].valueAuthor);
+	let showValuePriority = document.createTextNode(tabBooks[i].valuePriority);
+	let showValueCategory = document.createTextNode(tabBooks[i].valueCategory);
+
+	liTitle.appendChild(showValueTitle);
+	liAuthor.appendChild(showValueAutor);
+	liPriority.appendChild(showValuePriority);
+	liCategory.appendChild(showValueCategory);
+	
+	document.getElementById("showTitle").appendChild(liTitle);
+	document.getElementById("showAuthor").appendChild(liAuthor);
+	document.getElementById("showPriority").appendChild(liPriority);
+	document.getElementById("showCategory").appendChild(liCategory);
 }
 
 
